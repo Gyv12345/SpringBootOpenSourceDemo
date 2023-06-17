20c9b12ebdba26244cb09bd8dcad75d0a5c813a7
shichenyang <gyv12345@163.com>
2023-06-17 11:31:03 +0800

    feat: demo

---
+++ spring-cloud-tencent-demo/spring-cloud-tencent-dubbo/src/main/java/cn/yt4j/tencent/TencentApplication.java
@@ -0,0 +1,11 @@
+package cn.yt4j.tencent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TencentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TencentApplication.class,args);
+    }
+}
